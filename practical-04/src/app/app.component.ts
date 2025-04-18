eee7282fb7e7ea8cd6bd7fb2987da7d7ed88ddf8
devmehta-simform <dev.mehta@simformsolutions.com>
2025-04-18 18:55:19 +0530

    feat: add basic accordion component

--- practical-04/src/app/app.component.ts
+++ practical-04/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AccordionComponent } from './components/accordion/accordion.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AccordionComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'practical-04';
