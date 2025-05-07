1518a7c6944f11ea42b5577fda1020e9884144c1
devmehta-simform <dev.mehta@simformsolutions.com>
2025-05-07 13:27:01 +0530

    chore: init practical-05

---
+++ practical-05/src/app/components/user-form-modal/user-form-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-form-modal',
+  imports: [],
+  templateUrl: './user-form-modal.component.html',
+  styleUrl: './user-form-modal.component.css'
+})
+export class UserFormModalComponent {
+
+}
