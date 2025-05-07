9a31123fd4138fa5000cea88d72409103d400c7a
devmehta-simform <dev.mehta@simformsolutions.com>
2025-05-07 18:30:15 +0530

    feat: handle user creation

--- practical-05/src/app/components/user-form-modal/user-form-modal.component.ts
+++ practical-05/src/app/components/user-form-modal/user-form-modal.component.ts
@@ -1,9 +1,73 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { User } from '../../types/user';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-user-form-modal',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './user-form-modal.component.html',
   styleUrl: './user-form-modal.component.css',
 })
-export class UserFormModalComponent {}
+export class UserFormModalComponent {
+  @ViewChild('closeBtn') closeBtn!: ElementRef;
+  @Output() create = new EventEmitter<User>();
+
+  userForm = new FormGroup({
+    name: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    department: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    email: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.pattern(
+          /(?=.*\d)(?=.*[$&+,:;=?@#|'<>.^*()%!-])(?=.*[a-z])(?=.*[A-Z]).{8,16}/
+        ),
+      ],
+    }),
+    designation: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    salary: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+  });
+
+  handleUserCreate() {
+    if (this.userForm.valid) {
+      this.create.emit({
+        ...this.userForm.getRawValue(),
+        createdAt: new Date(),
+      });
+      if (this.closeBtn.nativeElement instanceof HTMLButtonElement) {
+        this.closeBtn.nativeElement.click();
+      }
+      this.userForm.reset();
+    } else {
+      this.userForm.markAllAsTouched();
+    }
+  }
+}
--- practical-05/src/app/components/user-table/user-table.component.ts
+++ practical-05/src/app/components/user-table/user-table.component.ts
@@ -1,60 +1,23 @@
 import { Component } from '@angular/core';
 import { User } from '../../types/user';
 import { UserFormModalComponent } from '../user-form-modal/user-form-modal.component';
+import { UserService } from '../../services/user.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-user-table',
-  imports: [UserFormModalComponent],
+  imports: [UserFormModalComponent, DatePipe],
   templateUrl: './user-table.component.html',
   styleUrl: './user-table.component.css',
 })
 export class UserTableComponent {
-  showUserFormModal() {}
-  users: User[] = [
-    {
-      name: 'Alice Johnson',
-      department: 'Engineering',
-      email: 'alice.johnson@example.com',
-      password: 'Alice@1234',
-      designation: 'Software Engineer',
-      salary: '90000',
-      createdAt: new Date('2024-11-10'),
-    },
-    {
-      name: 'Bob Smith',
-      department: 'Marketing',
-      email: 'bob.smith@example.com',
-      password: 'Bob$2024',
-      designation: 'Marketing Manager',
-      salary: '75000',
-      createdAt: new Date('2025-01-15'),
-    },
-    {
-      name: 'Charlie Lee',
-      department: 'Sales',
-      email: 'charlie.lee@example.com',
-      password: 'Char!ie123',
-      designation: 'Sales Executive',
-      salary: '68000',
-      createdAt: new Date('2024-09-01'),
-    },
-    {
-      name: 'Diana Patel',
-      department: 'Human Resources',
-      email: 'diana.patel@example.com',
-      password: 'Diana#2025',
-      designation: 'HR Specialist',
-      salary: '72000',
-      createdAt: new Date('2025-02-20'),
-    },
-    {
-      name: 'Ethan Williams',
-      department: 'Finance',
-      email: 'ethan.williams@example.com',
-      password: 'Ethan*999',
-      designation: 'Accountant',
-      salary: '83000',
-      createdAt: new Date('2024-12-05'),
-    },
-  ];
+  users: User[] = [];
+
+  constructor(private userService: UserService) {
+    this.users = this.userService.getAllUsers();
+  }
+
+  createUser(user: User) {
+    this.users = this.userService.createUser(user);
+  }
 }
