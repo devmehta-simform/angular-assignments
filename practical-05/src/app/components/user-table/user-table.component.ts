29efcfcf8bbbe68c923cd1a3f7d404eabe7c0489
devmehta-simform <dev.mehta@simformsolutions.com>
2025-05-07 18:58:18 +0530

    feat: add basic pagination

--- practical-05/src/app/components/user-table/user-table.component.ts
+++ practical-05/src/app/components/user-table/user-table.component.ts
@@ -3,15 +3,18 @@ import { User } from '../../types/user';
 import { UserFormModalComponent } from '../user-form-modal/user-form-modal.component';
 import { UserService } from '../../services/user.service';
 import { DatePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-table',
-  imports: [UserFormModalComponent, DatePipe],
+  imports: [UserFormModalComponent, DatePipe, FormsModule],
   templateUrl: './user-table.component.html',
   styleUrl: './user-table.component.css',
 })
 export class UserTableComponent {
   users: User[] = [];
+  pageNumber = 1;
+  pageOffset = 3;
 
   constructor(private userService: UserService) {
     this.users = this.userService.getAllUsers();
