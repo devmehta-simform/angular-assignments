877d2fc7d89ebb7b8161760c4b6cf03ef1ec40f7
devmehta-simform <dev.mehta@simformsolutions.com>
2025-05-13 10:35:50 +0530

    feat: add pagination

--- practical-07/src/app/components/post-list/post-list.component.ts
+++ practical-07/src/app/components/post-list/post-list.component.ts
@@ -5,18 +5,41 @@ import { Post } from '../../types/post';
 import { PostService } from '../../services/post.service';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-post-list',
-  imports: [RouterLink, PostListItemComponent, AsyncPipe],
+  imports: [RouterLink, PostListItemComponent, AsyncPipe, FormsModule],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.css',
 })
 export class PostListComponent {
-  posts$: Observable<Post[]>;
+  posts$!: Observable<{ posts: Post[]; totalItems: number }>;
+  page: number = 1;
+  pageOffset: number = 10;
 
   constructor(private postService: PostService) {
-    this.posts$ = this.postService.getAllPosts();
+    this.posts$ = this.postService.getAllPosts(this.page, this.pageOffset);
+  }
+
+  getPreviousPage(totalItems: number) {
+    this.page = this.page - 1;
+    this.goToPage(totalItems);
+  }
+
+  getNextPage(totalItems: number) {
+    this.page = this.page + 1;
+    this.goToPage(totalItems);
+  }
+
+  goToPage(totalItems: number) {
+    if (this.page >= 1 && this.page <= Math.ceil(totalItems / this.pageOffset))
+      this.posts$ = this.postService.getAllPosts(this.page, this.pageOffset);
+    else alert('invalid page number');
+  }
+
+  getTotalNumberOfPages(totalItems: number) {
+    return Math.ceil(totalItems / this.pageOffset);
   }
 
   handleDeletePost(id: number) {
--- practical-07/src/app/services/post.service.ts
+++ practical-07/src/app/services/post.service.ts
@@ -2,16 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Post } from '../types/post';
-import { mergeMap } from 'rxjs';
+import { map, mergeMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostService {
+  page = 1;
+  pageOffset = 10;
   constructor(private httpClient: HttpClient) {}
 
-  getAllPosts() {
-    return this.httpClient.get<Post[]>(environment.POSTS_API_URL);
+  getAllPosts(page: number, pageOffset: number) {
+    return this.httpClient.get<{ posts: Post[]; totalItems: number }>(
+      environment.POSTS_API_URL + `?page=${page}&pageOffset=${pageOffset}`
+    );
   }
 
   getPostById(id: number) {
@@ -29,7 +33,7 @@ export class PostService {
   deletePostById(id: number) {
     return this.httpClient.delete(environment.POSTS_API_URL + '/' + id).pipe(
       mergeMap(() => {
-        return this.getAllPosts();
+        return this.getAllPosts(this.page, this.pageOffset);
       })
     );
   }
